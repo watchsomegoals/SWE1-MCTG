7e9d2ab561532a4493d2f485a340a2e9b7057b40
Valentin Caraba <if18b122@technikum-wien.at>
2020-11-19 23:48:47 +0100

    Changed monster class so that it implements ICard interface

--- MCTG/MCTG/ACard.cs
+++ MCTG/MCTG/ACard.cs
@@ -4,28 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public enum ElementType 
-{   
-    fire = 1,
-    water = 2,
-    normal = 3
-}
-
 namespace MCTG
 {
     abstract class ACard
     {
         private static readonly Random _random = new Random();
+        protected CardType cardType;
+        protected ElementType elemType;
+        protected MonsterType monsterType;
         protected int damageValue;
-        protected ElementType type;
-
-        public ACard()
+        protected bool dead;
+  
+        public ACard(CardType _cardType, MonsterType _monsterType)
         {
-            this.type = getRandomElementType(_random.Next(1, 4));
+            this.elemType = GetRandomElementType(_random.Next(1, 4));
             this.damageValue = _random.Next(0, 101);
+            this.dead = false;
+            this.cardType = _cardType;
+            this.monsterType = _monsterType;
         }
 
-        public static ElementType getRandomElementType(int number)
+        public static ElementType GetRandomElementType(int number)
         {
             if (number == 1)
             {
@@ -44,15 +43,41 @@ namespace MCTG
                 throw new ArgumentException("Argument is not between 1 and 3!");
             }
         }
-
         public int DamageValue
         {
             get { return damageValue; }
+            set { damageValue = value; }
         }
-
-        public ElementType Type
+        public ElementType ElemType
         {
-            get { return type; }
+            get { return elemType; }
+        }
+        public bool Dead
+        {
+            get { return dead; }
+            set { dead = value; }
+        }
+        public CardType CType
+        {
+            get { return cardType; }
+        }
+        public MonsterType MType
+        {
+            get { return monsterType; }
+        }
+        public abstract int Attack(ACard other);
+        //return 1 if attack was successful
+        //return 0 if attack wasn't successful
+        public int NormalAttack(int att, int def)
+        {
+            if(att > def)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
