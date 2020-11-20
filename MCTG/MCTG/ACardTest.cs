6e3852cb5bdbfcaf99cb6cb8180ef9be9ffb4aa7
Valentin Caraba <if18b122@technikum-wien.at>
2020-11-20 01:37:53 +0100

    Initial commit

--- MCTG/MCTG/ACardTest.cs
+++ MCTG/MCTG/ACardTest.cs
@@ -10,15 +10,56 @@ namespace MCTG
     [TestFixture]
     class ACardTest
     {
+        //arrange
         ElementType testType;
 
         [Test]
         [Category("pass")]
-        public void getRandomElementType()
+        [TestCase(1)]
+        public void GetRandomElementTypeFire(int a)
         {
-            testType = ACard.getRandomElementType(1);
+            //act
+            testType = ACard.GetRandomElementType(a);
 
+            //verify
             Assert.AreEqual(ElementType.fire, testType);
+        }
+
+        [Test]
+        [Category("pass")]
+        [TestCase(2)]
+        public void GetRandomElementTypeWater(int a)
+        {
+            //act
+            testType = ACard.GetRandomElementType(a);
+
+            //verify
+            Assert.AreEqual(ElementType.water, testType);
+        }
+
+        [Test]
+        [Category("pass")]
+        [TestCase(3)]
+        public void GetRandomElementTypeNormal(int a)
+        {
+            //act
+            testType = ACard.GetRandomElementType(a);
+
+            //verify
+            Assert.AreEqual(ElementType.normal, testType);
+        }
+
+        [Test]
+        [Category("fail")]
+        [TestCase(4)]
+        public void GetRandomElementTypeFail(int a)
+        {
+            //act
+            //testType = ACard.GetRandomElementType(a);
+
+            //verify
+            var ex = Assert.Throws<ArgumentException>(() => ACard.GetRandomElementType(a));
+            Assert.That(ex.Message, Is.EqualTo("Argument is not between 1 and 3!"));
         }
     }
 }
